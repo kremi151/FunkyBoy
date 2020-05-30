71a875cd9133455b41ceb52f0e340667245167d0
Michel Kremer <kremi151dev@gmail.com>
2020-05-30 23:40:19 +0200

    Move useful resources to README

---
+++ core/README.md
@@ -0,0 +1,18 @@
+# FunkyBoy
+
+## Useful resources
+
+https://www.pastraiser.com/cpu/gameboy/gameboy_opcodes.html
+http://www.devrs.com/gb/files/opcodes.html
+https://rednex.github.io/rgbds/gbz80.7.html
+https://www.reddit.com/r/EmuDev/comments/7ljc41/how_to_algorithmically_parse_gameboy_opcodes/
+https://www.reddit.com/r/EmuDev/comments/4clh23/trouble_with_halfcarrycarry_flag/
+https://gbdev.gg8.se/wiki/articles/The_Cartridge_Header
+https://gbdev.gg8.se/wiki/articles/Gameboy_ROM_Header_Info
+http://z80-heaven.wikidot.com/instructions-set
+http://tutorials.eeems.ca/Z80ASM/app1d.htm
+http://www.z80.info/z80sflag.htm
+
+## Test ROMS
+
+https://gbdev.gg8.se/wiki/articles/Test_ROMs
--- core/source/emulator/cpu.cpp
+++ core/source/emulator/cpu.cpp
@@ -92,23 +92,6 @@ inline void addWithCarry(u8 &a, u8 &s, bool carry) {
 }
 
 bool CPU::doTick() {
-    // Useful resources:
-    // https://www.pastraiser.com/cpu/gameboy/gameboy_opcodes.html
-    // http://www.devrs.com/gb/files/opcodes.html
-    // https://rednex.github.io/rgbds/gbz80.7.html
-    // https://www.reddit.com/r/EmuDev/comments/7ljc41/how_to_algorithmically_parse_gameboy_opcodes/
-    // https://www.reddit.com/r/EmuDev/comments/4clh23/trouble_with_halfcarrycarry_flag/
-    // https://gbdev.gg8.se/wiki/articles/The_Cartridge_Header
-    // https://gbdev.gg8.se/wiki/articles/Gameboy_ROM_Header_Info
-    // http://z80-heaven.wikidot.com/instructions-set
-    // http://tutorials.eeems.ca/Z80ASM/app1d.htm
-    // http://www.z80.info/z80sflag.htm
-
-    // For test ROMs:
-    // https://gbdev.gg8.se/wiki/articles/Test_ROMs
-
-    // TODO: Remove logs
-
     auto opcode = memory->read8BitsAt(progCounter++);
 
     debug_print("> instr 0x%02X\n", opcode);
