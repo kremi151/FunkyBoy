ccd6ad1d9ddcb5673aee9367ba88da484be64367
Michel Kremer <kremi151dev@gmail.com>
2020-07-29 20:41:48 +0200

    Create SDL platform (#11)

---
+++ platform-sdl/README.md
@@ -0,0 +1,12 @@
+# FunkyBoy - SDL2
+
+This is the implementation of FunkyBoy for Windows, Linux and macOS
+
+## Development notes (Windows)
+
+1. Download SDL2:
+https://www.libsdl.org/release/SDL2-devel-2.0.12-VC.zip
+2. Extract the ZIP somewhere
+3. Add a `SDL2_DIR` CMake variable which points to the folder contained in the extracted archive (the full path of the `SDL2-2.0.12` folder)
+4. Add the the full path of `SDL2-2.0.12/lib/x64` to the `PATH`
+5. Ensure that you're building for the correct architecture (just saying, I was building for `x86` without noticing...)
---
+++ platform-sdl/source/main.cpp
@@ -0,0 +1,40 @@
+/**
+ * Copyright 2020 Michel Kremer (kremi151)
+ *
+ * Licensed under the Apache License, Version 2.0 (the License);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <SDL.h>
+
+int main() {
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window *window = SDL_CreateWindow(
+            "SDL2Test",
+            SDL_WINDOWPOS_UNDEFINED,
+            SDL_WINDOWPOS_UNDEFINED,
+            640,
+            480,
+            0
+    );
+
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+
+    SDL_Delay(3000);
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 420;
+}
