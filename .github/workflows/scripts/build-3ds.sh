851559a7fd478d252ef3f4c7b51c504844bc96b0
Michel Kremer <kremi151dev@gmail.com>
2020-08-14 23:52:04 +0200

    Upload built 3DS app

--- .github/workflows/scripts/build-3ds.sh
+++ .github/workflows/scripts/build-3ds.sh
@@ -5,7 +5,7 @@ new_cmake_dir="$HOME/tmp_cmake"
 mkdir $new_cmake_dir
 cd $new_cmake_dir
 wget https://cmake.org/files/v3.18/cmake-3.18.1-Linux-x86_64.tar.gz
-tar -xzvf cmake-3.18.1-Linux-x86_64.tar.gz
+tar -xzf cmake-3.18.1-Linux-x86_64.tar.gz
 export PATH="$new_cmake_dir/cmake-3.18.1-Linux-x86_64/bin:$PATH"
 
 echo "Test CMake installation"
@@ -14,7 +14,7 @@ cmake --version
 
 echo "Build 3DS platform"
 cd /github/workspace/platform-3ds
-mkdir cmake-build-3ds
-cd cmake-build-3ds
+mkdir cmake-build
+cd cmake-build
 cmake ..
 make
