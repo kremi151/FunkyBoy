12b651d8bc18755d1912c46c6a94dcf23f633811
Michel Kremer <kremi151dev@gmail.com>
2020-08-14 23:38:52 +0200

    Upgrade CMake in 3DS workflow

--- .github/workflows/scripts/build-3ds.sh
+++ .github/workflows/scripts/build-3ds.sh
@@ -1,5 +1,22 @@
 #!/bin/sh
 
+tmp_dir=$(mktemp -d -t ci-XXXXXXXXXX)
+
+echo "Upgrade to newer CMake"
+sudo apt-get purge cmake
+cd $tmp_dir
+wget https://github.com/Kitware/CMake/releases/download/v3.18.1/cmake-3.18.1.tar.gz
+tar -xzvf cmake-3.18.1.tar.gz
+cd cmake-3.18.1
+./bootstrap
+make -j4
+sudo make install
+
+echo "Test CMake installation"
+cd ~
+cmake --version
+
+echo "Build 3DS platform"
 cd /github/workspace/platform-3ds
 mkdir cmake-build-3ds
 cd cmake-build-3ds
